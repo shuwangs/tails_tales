8fad214fbb28c870b901c3d4884db5dbe08cabe8
shu.wang <60482034+shuwangs@users.noreply.github.com>
2026-04-01 20:55:17 +0000

    style: navbar styling

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,11 +1,14 @@
 const NavBar = () => {
 	return (
-		<nav className="fix flex flex-row items-center justify-between p-4 bg-[var(--color-primary-border)]">
-			<h1>Bobo's Diary</h1>
-			<div className="flex flex-row">
-				<a href="/entries">Home</a>
-				<a href="/write">Write</a>
+		<nav className="w-full h-20 fixed flex flex-row bg-amber-100/95 border-b-2 border-amber-200">
+			<div className="flex flex-row w-full max-w-[1126px] mx-auto items-center justify-between ">
+				<h1 className="color-primary-text font-heading">Bobo's Diary</h1>
+				<div className="flex flex-row gap-4">
+					<a href="/entries">Home</a>
+					<a href="/write">Write</a>
+				</div>
 			</div>
+
 		</nav>
 	);
 };
--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -1,6 +1,6 @@
 const MainLayout = ({ children }) => {
     return (
-        <div className="flex flex-col max-w-[1126px] mx-auto">        
+        <div className="flex flex-col max-w-[1126px] mx-auto pt-20">        
             {children}
         </div>
     );
