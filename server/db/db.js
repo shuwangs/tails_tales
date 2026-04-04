c7a4f3b10d331fed7c1c48076150be47bc885537
shu.wang <shuw425@gmail.com>
2026-04-04 15:44:07 -0400

    format-fix:

--- server/db/db.js
+++ server/db/db.js
@@ -3,11 +3,17 @@ import { Pool } from "pg";
 
 dotenv.config();
 
-const pool = new Pool({
-	host: process.env.PGHOST,
-	user: process.env.PGUSER,
-	database: process.env.PGDATABASE,
-	port: process.env.PGPORT,
-});
-
+let pool;
+if (process.env.db_connection) {
+	pool = new Pool({
+		connectionString: process.env.db_connection,
+	});
+} else {
+	pool = new Pool({
+		host: process.env.PGHOST,
+		user: process.env.PGUSER,
+		database: process.env.PGDATABASE,
+		port: process.env.PGPORT,
+	});
+}
 export default pool;
