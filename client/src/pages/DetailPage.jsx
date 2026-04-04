1c3498dabe6c333ee0f1a8b6cecd9104213f54a5
Luis Gonzalez <luis.gonzalez@airbnb.com>
2026-04-04 13:34:00 -0700

    Some bad change

--- client/src/pages/DetailPage.jsx
+++ client/src/pages/DetailPage.jsx
@@ -8,6 +8,7 @@ const DetailPage = () => {
 	const { entries } = useEntries();
 
 	const selectedEntry = entries.find((entry) => entry.id === Number(id));
+	const badchange = "some text"
 
 	return (
 		<MainLayout>
