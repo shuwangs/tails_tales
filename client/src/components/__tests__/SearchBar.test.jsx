04413d14e7aecb4403ae4b104575021fdfda381f
shu.wang <shuw425@gmail.com>
2026-04-05 15:45:16 -0400

    test: add test to check form is display for WriteForm

--- client/src/components/__tests__/SearchBar.test.jsx
+++ client/src/components/__tests__/SearchBar.test.jsx
@@ -1,16 +1,15 @@
-import "@testing-library/jest-dom"
+import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import SearchBar from "../SearchBar";
 import { describe, expect, it } from "vitest";
 
 describe("SearchBar test", () => {
-    it("renders search input ", () => {
-        render(<SearchBar />);
+	it("renders search input ", () => {
+		render(<SearchBar />);
 
-        const input = screen.getByPlaceholderText("Seaching for diary");
+		const input = screen.getByPlaceholderText("Seaching for diary");
 
-        expect(input).toBeInTheDocument();
-        expect(input).toHaveAttribute("type", "text");
-
-    })
-})
+		expect(input).toBeInTheDocument();
+		expect(input).toHaveAttribute("type", "text");
+	});
+});
---
+++ client/src/components/__tests__/WriteForm.test.jsx
@@ -0,0 +1,54 @@
+import "@testing-library/jest-dom/vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
+import WriteForm from "../WriteForm.jsx";
+import { useEntries } from "../../contexts/EntriesContext.jsx";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+vi.mock("../../contexts/EntriesContext.jsx", () => ({
+    useEntries: vi.fn(),
+}));
+
+vi.mock("react-router-dom", async () => {
+    const actual = await vi.importActual("react-router-dom");
+    return {
+        ...actual,
+        useNavigate: vi.fn(),
+    };
+});
+
+vi.mock("react-toastify", () => ({
+    toast: {
+        error: vi.fn(),
+    },
+}));
+
+vi.mock("../../AiCard.jsx", () => ({
+    default: () => <div>AI Card</div>,
+}));
+
+describe("WriteForm test", () => {
+    const mockNavigate = vi.fn();
+    const mockAddEntry = vi.fn();
+    vi.clearAllMocks();
+
+    useNavigate.mockReturnValue(mockNavigate);
+
+    useEntries.mockReturnValue({
+        petId: 1,
+        error: null,
+        loading: false,
+        addEntry: mockAddEntry,
+    });
+
+    it("renders form fields and buttons", () => {
+        render(<WriteForm />);
+
+        expect(
+            screen.getByRole("heading", { name: /another day/i }),
+        ).toBeInTheDocument();
+    });
+});
+
+// REFERENCE: https://medium.com/@1992season/a-way-to-mock-and-test-navigation-events-in-react-by-vitest-c59f9c8ccc0b
