3c8fc76932ef5981753ec3496025b2510dd99bba
shu.wang <shuw425@gmail.com>
2026-04-08 01:18:35 -0400

    feat(ai): implement embedding utility using MiniLM model - add embedText using all-MiniLM-L2-v2 - add a simple pipeline init - set pooling to mean and normalize to be true to make the vector 1 dimension - add a testcase for embedText

--- client/src/utils/embedText.js
+++
@@ -1,9 +0,0 @@
-
-export const embedText = (text) => {
-    if (!text || !text.trim()) {
-        throw new Error("text cannot be emtpy")
-    }
-
-    return Array(1536).fill(0.01);
-
-}
---
+++ server/utils/embedText.js
@@ -0,0 +1,31 @@
+import { pipeline } from "@huggingface/transformers";
+
+let extractor = null;
+
+const getExtractor = async () => {
+    if (!extractor) {
+        extractor = await pipeline(
+            "feature-extraction",
+            "Xenova/all-MiniLM-L6-v2"
+        );
+    }
+    return extractor;
+};
+const truncateText = (text, maxLength = 1000) => {
+    return text.length > maxLength ? text.slice(0, maxLength) : text;
+};
+
+export const embedText = async (text) => {
+    if (!text || !text.trim()) {
+        throw new Error("text cannot be emtpy")
+    }
+    const model = await getExtractor()
+
+    const result = await model(truncateText(text),
+        {
+            pooling: "mean", normalize: true
+        }
+    )
+    return Array.from(result.data);
+
+}
